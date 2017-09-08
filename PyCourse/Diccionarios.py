9931efbb20c634f5dc74d7dd2fddd26fa276dc50
MagicElyas <elias2603@hotmail.com>
2017-09-08 16:22:36 +0200

    Diccionarios
    Aprendo a usar las estructuras de datos diccionario, que viene implementada en py, es bastante util porque a diferencia de hs podemos tener distintos tipos de datos tanto en claves como en valores.

---
+++ PyCourse/Diccionarios.py
@@ -0,0 +1,16 @@
+# Esto es una tupla t= (True,12)
+
+#Esto es un diccionario
+d = { "A":[1,2,3,4] , "B" : ('b','c','d') }
+
+#Para indexar los diccionarios y buscar los values accedemos por keys
+print  d
+
+print d["A"]
+
+
+# Indexar value nuevo por la clave
+
+d["A"] = [0]
+
+print d
