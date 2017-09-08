7bad982ee426efb80c676c6e645c28c485300539
MagicElyas <elias2603@hotmail.com>
2017-09-08 16:50:01 +0200

    Condicionales.py
    hago una serie de pruebas con las sentencias condicionales

---
+++ PyCourse/Condicionales.py
@@ -0,0 +1,12 @@
+edad=17
+
+if edad<0:
+        print "Error: edad no valida"
+else:
+    print "Edad valida"
+    if edad >= 18:
+        print "Eres mayor de edad"
+    else:
+        print "Eres menor de edad"
+
+print "Control"
