f8f19d50232e251aea2ead6bd86bef8ad134715c
MagicElyas <elias2603@hotmail.com>
2017-09-08 15:34:51 +0200

    Enteros.py y cademas.,py
    siguiendo un tutorial de internet he aprendido las cosas basicas de la representacion de numeros y de cadenas ademas de los booleanos, todo sin mucha complicacion

---
+++ PyCourse/cadenas.py
@@ -0,0 +1,35 @@
+#Comillas SImples
+cads= 'Me llamo pepe\n'
+#COmillas dobles
+cadd= "Estoy aprendiendo python"
+
+cadc= """\nEstoy probando
+a dar formato a un texto
+    con esta representacion
+de comillas triples
+"""
+
+#La funcion type nos devuelve el tipo de la variable que le pasamos como argumento
+
+
+print (cads)
+print (cadd)
+print (cadc)
+
+# vamos a concatenar cadenas
+cad="Cadena1 " #*3
+print (cad)
+
+cad2= "Cadena 2 " # *5
+print (cad2)
+
+
+res=cad+cad2
+print ("El resultado es: "+res)
+
+
+# BOOLEAN
+b1=True
+b2=False
+
+print (not b2)
---
+++ PyCourse/enteros.py
@@ -0,0 +1,8 @@
+e = 5 # Entero
+e = 5L #Long
+
+real = 0.567 # FLOAT
+real = 567e-3
+
+print (real)
+print (e)
