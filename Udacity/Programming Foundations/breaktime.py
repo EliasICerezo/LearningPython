6852285f06a3a7e7e708e3a6e56b9387dc10f632
MagicElyas <elias2603@hotmail.com>
2017-10-12 12:10:47 +0200

    Breaktime.py
    1st program of the course on udacity, very simple, but it shows me things that i dont know like the webbrowser funct.

---
+++ Udacity/Programming Foundations/breaktime.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+
+num=0
+
+print("Hey i am on")
+while(num<3):
+    time.sleep(7200)
+    webbrowser.open("https://github.com/MagicElyas")
+    num=num+1
+
+print ("Ive ended")
